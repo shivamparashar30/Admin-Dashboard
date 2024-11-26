66e370fc1cb9a22b55313ae1534205dc43c63156
shivam-parashar <shivam.tigbar@gmail.com>
2024-11-27 02:48:11 +0530

    Initial commit

---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Common/Header";
+
+const OverviewPage = () => {
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Overview" />
+    </div>
+  );
+};
+
+export default OverviewPage;
